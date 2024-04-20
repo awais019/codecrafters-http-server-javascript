ef00820e1d7150ba4d613880dcd1196148cc3167
Muhammad Awais <muhammad.awais.20024@gmail.com>
2024-04-20 18:30:05 +0500

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -4,11 +4,11 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//     server.close();
-//   });
-// });
-//
-// server.listen(4221, "localhost");
+const server = net.createServer((socket) => {
+  socket.on("close", () => {
+    socket.end();
+    server.close();
+  });
+});
+
+server.listen(4221, "localhost");
